97ac076d4488d390967ee30141be1123a91aca62
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2024-10-30 02:05:42 -0400

    + online simulation for other methods

--- examples/simulation/step3_run_simulation.py
+++ examples/simulation/step3_run_simulation.py
@@ -288,6 +288,102 @@ def simulate_heuristic_offline(scheduling_method: SchedulingMethod, initial_quer
     simulator.plot_request_latency(ignore_initialize=True, save_path=None)
 
 
+def simulate_heuristic_online(scheduling_method: SchedulingMethod, avg_throughput: float):
+    # ---------------------------------------- Initialization ---------------------------------------- #
+    # load the model placement
+    # cluster_file_path is "simulator_cluster_file_name" in layout_args
+    machine_num_dict = {"A100": 4, "L4": 8, "T4": 12}
+    layout_synthesizer = LayoutSynthesizer(
+        complete_cluster_file_name="config/single24.ini",
+        machine_profile_name="config/machine_profile.ini",
+        model_name=ModelName.LLaMa70B,
+        workspace_path="./sim_files",
+        layout_method=LayoutMethod.LoadExisting,
+        machine_num_dict=machine_num_dict
+    )
+    layout_args = {
+        "solution_file_name": "./layouts/ilp/ilp_sol.ini",
+        "simulator_cluster_file_name": "./layouts/ilp/simulator_cluster.ini",
+    }
+    cluster_file_path = layout_synthesizer.synthesize(args=layout_args)
+
+    # initialize the simulator
+    simulator = ClusterSimulator(model_name=ModelName.LLaMa70B, machine_num_dict=machine_num_dict)
+    simulator.from_ini_file(config_file_name=cluster_file_path)
+    simulator.init_scheduler(scheduling_method=scheduling_method, args=None)
+    simulator.init_query_manager()
+    simulator.mark_as_ready()
+
+    # load the models into the simulator and update scheduler
+    finish_model_loading_time = layout_synthesizer.set_layout(simulator=simulator)
+    simulator.update_scheduler()
+
+    # print some status information
+    print(f"Upper bound of throughput: {layout_synthesizer.layout_synthesizer.get_flow_upper_bound()}")
+
+    # ------------------------------------------ Simulation ------------------------------------------ #
+    # setup simulation and run
+    warm_up, duration = 0, 1800
+    auto_test = OnlineRequestFeeder(cluster_token_throughput=avg_throughput,
+                                    start_time=finish_model_loading_time,
+                                    duration=duration, seed=0)
+    auto_test.auto_simulate(simulator=simulator, watch_items=["all"], watch_interval=10)
+
+    # ------------------------------------------- Analysis ------------------------------------------- #
+    analysis_start_time = finish_model_loading_time + warm_up
+    analysis_end_time = finish_model_loading_time + warm_up + duration
+
+    # compute decode throughput
+    total_tokens = 0
+    for request_uid, time_request in simulator.finished_requests.items():
+        time, request = time_request
+        if request.phase == RequestPhase.Initialization:
+            continue
+        if analysis_start_time <= time <= analysis_end_time:
+            assert request.token_seq_length == 1, "Only count decode requests!"
+            total_tokens += request.token_seq_length
+    decode_throughput = total_tokens / duration
+
+    # compute and save prompt and decode latency
+    prompt_latency_list, decode_latency_list = [], []
+    sum_prompt_latency, sum_decode_latency = 0, 0
+    valid_prompts, valid_decodes = 0, 0
+    for request_uid, time_request in simulator.finished_requests.items():
+        time, request = time_request
+        if analysis_start_time <= time <= analysis_end_time:
+            if request.phase == RequestPhase.Initialization:
+                sum_prompt_latency += request.location_history[-1][1] - request.location_history[0][1]
+                prompt_latency_list.append((time, request.location_history[-1][1] - request.location_history[0][1]))
+                valid_prompts += 1
+            elif request.phase == RequestPhase.Increment:
+                sum_decode_latency += request.location_history[-1][1] - request.location_history[0][1]
+                decode_latency_list.append((time, request.location_history[-1][1] - request.location_history[0][1]))
+                valid_decodes += 1
+            else:
+                assert False, "Found unknown requests phase!"
+    avg_prompt_latency = sum_prompt_latency / valid_prompts
+    avg_decode_latency = sum_decode_latency / valid_decodes
+
+    # print and plot (we don't save the plots here)
+    print(f"# ------------------------------------------------------------- #")
+    print(f"Simulation Results (time range: {analysis_start_time}s - {analysis_end_time}s)")
+    print(f"Avg decode speed: {decode_throughput:.1f} tokens/s")
+    print(f"Avg prompt latency: {avg_prompt_latency:.3f}s")
+    print(f"Avg decode latency: {avg_decode_latency:.3f}s")
+    print(f"# ------------------------------------------------------------- #")
+    simulator.plot_inference_speed(max_time=700)
+    simulator.plot_request_latency(ignore_initialize=True)
+
+    # save the latency data with pickle
+    # import pickle
+    # prompt_file_name = "prompt_latency.pkl"
+    # decode_file_name = "decode_latency.pkl"
+    # with open(prompt_file_name, "wb") as f:
+    #     pickle.dump(prompt_latency_list, f)
+    # with open(decode_file_name, "wb") as f:
+    #     pickle.dump(decode_latency_list, f)
+
+
 def main():
     """
     Finally, we can run the simulator to see how the model placement and request scheduling perform.
@@ -301,21 +397,50 @@ def main():
             # Scheduling method: Helix MaxFlow
             # Request arrival pattern: offline
             simulate_maxflow_offline()
+
         elif scheduler == "swarm":
             # Scheduling method: Swarm
             # Request arrival pattern: offline
             simulate_heuristic_offline(scheduling_method=SchedulingMethod.Swarm, initial_query_num=240)
+
         elif scheduler == "random":
             # Scheduling method: Random
             # Request arrival pattern: offline
             simulate_heuristic_offline(scheduling_method=SchedulingMethod.Naive, initial_query_num=240)
+
         elif scheduler == "shortest_queue":
             # Scheduling method: Shortest Queue
             # Request arrival pattern: offline
             simulate_heuristic_offline(scheduling_method=SchedulingMethod.ShortestQueue,
                                        initial_query_num=240)
+
         else:
             raise ValueError("Unknown scheduler!")
+
+    elif mode == "online":
+        if scheduler == "maxflow":
+            # Scheduling method: Helix MaxFlow
+            # Request arrival pattern: online
+            simulate_maxflow_online(avg_throughput=700)
+
+        elif scheduler == "swarm":
+            # Scheduling method: Swarm
+            # Request arrival pattern: online
+            simulate_heuristic_online(scheduling_method=SchedulingMethod.Swarm, avg_throughput=400)
+
+        elif scheduler == "random":
+            # Scheduling method: Random
+            # Request arrival pattern: online
+            simulate_heuristic_online(scheduling_method=SchedulingMethod.Naive, avg_throughput=400)
+
+        elif scheduler == "shortest_queue":
+            # Scheduling method: Shortest Queue
+            # Request arrival pattern: online
+            simulate_heuristic_online(scheduling_method=SchedulingMethod.ShortestQueue, avg_throughput=400)
+
+        else:
+            raise ValueError("Unknown scheduler!")
+
     else:
         raise ValueError("Unknown mode!")
 
