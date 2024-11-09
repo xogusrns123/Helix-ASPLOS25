6709fa29c6c0aad3d43d15acbc33b27baf06977a
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2024-11-09 01:02:51 -0500

    + removed tests

--- artifact_evaluation/_test_ray_worker.py
+++
@@ -1,3 +0,0 @@
-import llm_host, llm_worker
-
-print(dir(llm_host), dir(llm_worker))
--- artifact_evaluation/launch_worker.sh
+++
@@ -1,3 +0,0 @@
-conda activate runtime
-cd /home/meiyixuan2000/helix/artifact_evaluation/single_cluster
-python3 _test_ray_worker.py # change this to the 'python step_x_xxx.py'
