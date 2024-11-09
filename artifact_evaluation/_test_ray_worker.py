05e8a21f8aeef9bd28c6a8de0d62837795497624
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2024-11-08 22:39:29 -0500

    + add test ray files

---
+++ artifact_evaluation/_test_ray_worker.py
@@ -0,0 +1,3 @@
+import llm_host, llm_worker
+
+print(dir(llm_host), dir(llm_worker))
---
+++ artifact_evaluation/launch_worker.sh
@@ -0,0 +1,3 @@
+conda activate runtime
+cd /home/meiyixuan2000/helix/artifact_evaluation/single_cluster
+python3 _test_ray_worker.py # change this to the 'python step_x_xxx.py'
