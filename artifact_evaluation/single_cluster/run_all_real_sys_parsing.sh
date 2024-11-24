f12f66e335856e4fe2a93fddcd43db5f309bbbbb
Yixuan Mei <30572462+AntonyMei@users.noreply.github.com>
2024-11-24 15:20:17 -0500

    + add run_all_real_sys_parsing.sh for single cluster setup

---
+++ artifact_evaluation/single_cluster/run_all_real_sys_parsing.sh
@@ -0,0 +1,56 @@
+echo "Parsing real system evaluation results for helix llama30b offline:"
+echo "Showing each sub-cluster separately"
+python step7_parse_results.py helix_a100 llama30b offline
+python step7_parse_results.py helix_l4 llama30b offline
+python step7_parse_results.py helix_t4 llama30b offline
+
+echo "Parsing real system evaluation results for helix llama30b online:"
+echo "Showing each sub-cluster separately"
+python step7_parse_results.py helix_a100 llama30b online
+python step7_parse_results.py helix_l4 llama30b online
+python step7_parse_results.py helix_t4 llama30b online
+python step7_parse_results.py helix llama30b online
+
+echo "Parsing real system evaluation results for swarm llama30b offline:"
+python step7_parse_results.py swarm llama30b offline
+
+echo "Parsing real system evaluation results for swarm llama30b online:"
+python step7_parse_results.py swarm llama30b online
+
+echo "Parsing real system evaluation results for separate pipelines llama30b offline:"
+echo "Showing each sub-cluster separately"
+python step7_parse_results.py separate_a100 llama30b offline
+python step7_parse_results.py separate_l4 llama30b offline
+python step7_parse_results.py separate_t4 llama30b offline
+
+echo "Parsing real system evaluation results for separate pipelines llama30b online:"
+echo "Showing each sub-cluster separately"
+python step7_parse_results.py separate_a100 llama30b online
+python step7_parse_results.py separate_l4 llama30b online
+python step7_parse_results.py separate_t4 llama30b online
+python step7_parse_results.py separate llama30b online
+
+echo "Parsing real system evaluation results for helix llama70b offline:"
+python step7_parse_results.py helix llama70b offline
+
+echo "Parsing real system evaluation results for helix llama70b online:"
+python step7_parse_results.py helix llama70b online
+
+echo "Parsing real system evaluation results for swarm llama70b offline:"
+python step7_parse_results.py swarm llama70b offline
+
+echo "Parsing real system evaluation results for swarm llama70b online:"
+python step7_parse_results.py swarm llama70b online
+
+echo "Parsing real system evaluation results for separate pipelines llama70b offline:"
+echo "Showing each sub-cluster separately"
+python step7_parse_results.py separate_a100 llama70b offline
+python step7_parse_results.py separate_l4 llama70b offline
+python step7_parse_results.py separate_t4 llama70b offline
+
+echo "Parsing real system evaluation results for separate pipelines llama70b online:"
+echo "Showing each sub-cluster separately"
+python step7_parse_results.py separate_a100 llama70b online
+python step7_parse_results.py separate_l4 llama70b online
+python step7_parse_results.py separate_t4 llama70b online
+python step7_parse_results.py separate llama70b online
