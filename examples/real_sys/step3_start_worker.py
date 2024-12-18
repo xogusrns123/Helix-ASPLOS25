19fbdae35d41ca93938d65db3f7269ca1ea56859
Taehyun Kim <kth@mango4.kaist.ac.kr>
2024-12-18 21:57:30 +0900

    develop

--- examples/real_sys/step3_start_worker.py
+++ examples/real_sys/step3_start_worker.py
@@ -17,7 +17,7 @@ def main():
     print(f"Starting worker with scheduling method: {scheduling_method}.")
 
     # run worker
-    run_worker(scheduling_method=scheduling_method, model_name="./model2")
+    run_worker(scheduling_method=scheduling_method, model_name="./Llama-2-7b-hf")
 
 
 if __name__ == '__main__':
