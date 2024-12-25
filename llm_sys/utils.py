ba2f34bcefc0c3e4841b3e7cf304d87d11d9f0b9
Taehyun Kim <kth@mango4.kaist.ac.kr>
2024-12-26 02:17:29 +0900

    develop

--- llm_sys/utils.py
+++ llm_sys/utils.py
@@ -16,7 +16,8 @@ def to_real(node_id: int) -> int:
 
 
 # CONFIG_BROADCAST_ADDR = "tcp://10.128.0.31:5000"
-CONFIG_BROADCAST_ADDR = "tcp://143.248.53.59:5000"
+# CONFIG_BROADCAST_ADDR = "tcp://143.248.53.59:5000"
+CONFIG_BROADCAST_ADDR = "tcp://143.248.53.112:5000"
 
 
 def warm_up():
