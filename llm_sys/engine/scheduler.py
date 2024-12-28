dc1604ace780eebef81c1e030ac6e6c9c465fd9e
Taehyun Kim <kth@mango4.kaist.ac.kr>
2024-12-28 16:07:57 +0900

    develop

--- llm_sys/engine/scheduler.py
+++ llm_sys/engine/scheduler.py
@@ -618,9 +618,9 @@ class LayerwiseScheduler(Scheduler):
     def update_req_data(self, layer_id: int, req_id: str, seq_datas: Dict[int, torch.Tensor]):  
         
         # FIXME currently, for conevenient experiment for disaggregate design
-        # if (req_id, layer_id) not in self.seq_groups:
-        #     print("decode other instances")
-        #     print(f"{(req_id, layer_id)}")
+        if (req_id, layer_id) not in self.seq_groups:
+            print("decode other instances")
+            print(f"{(req_id, layer_id)}")
         #     # print(f"{self.seq_groups}")
         #     sampling_params = SamplingParams()
         #     sampling_params.max_tokens = 300
